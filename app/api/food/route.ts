9f98ecdbb1c6b162e14655340840b503744e2834
25LP9846 25LP9846 <25LP9846@G301-CC0189.local>
2025-10-09 11:28:30 +0800

    some changes

--- app/api/food/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  return Response.json({ message: "hello from food" });
-}
-export async function POST() {
-  return Response.json({ message: "hello from food post" });
-}
