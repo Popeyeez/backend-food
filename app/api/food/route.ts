dc1258c43cd88a7f0db2a4f8cc39985f1c931292
Popeyeez <lozo022011@gmail.com>
2025-10-09 22:35:00 +0800

    Local changes before pulling

--- app/api/food/route.ts
+++ app/api/food/route.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-  return Response.json({ message: "hello from food" });
+  return Response.json({ data: "hello from food" });
 }
 export async function POST() {
   return Response.json({ message: "hello from food post" });
